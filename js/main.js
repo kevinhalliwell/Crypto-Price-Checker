b04fbf721fbca2fc15d3c8873d96568744e4373b
kevinhalliwell <godzirrasti@gmail.com>
2022-04-15 11:21:57 -0400

    updates
    initial build testing

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Coincode api used to get crypto prices
+
+function getFetch(){
+  	//const choice = document.querySelector('input').value
+  	//console.log(choice)
+  	const url = `api.coincap.io/v2/assets`
+
+  	fetch(url)
+      	.then(res => res.json()) // parse response as JSON
+      	.then(data => {
+      	console.log(data)
+      	})
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+getFetch()
