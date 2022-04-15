34ba4b8808ae61ce5964fc47a0fea9aa7b3c2e1e
kevinhalliwell <godzirrasti@gmail.com>
2022-04-15 11:56:26 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
 //Coincode api used to get crypto prices
-// var requestOptions = {
-// 	method: 'GET',
-// 	redirect: 'follow'
-// };
 
-// function getFetch(){
-//   	fetch("api.coincap.io/v2/assets", requestOptions)
-//       	.then(res => res.text())
-//       	.then(data => {
-//       	console.log(data)
-//       	})
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });
-// }
+function getFetch(){
+  	fetch("api.coincap.io/v2/assets")
+      	.then(res => res.json())
+      	.then(data => {
+      	console.log(data)
+      	})
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
 
-// getFetch()
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'manual'
-};
-
-fetch("api.coincap.io/v2/assets", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+getFetch()
