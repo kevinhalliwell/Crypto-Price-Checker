14f269d33df2cacef4c2d084c0d2f6fb2a408e35
kevinhalliwell <godzirrasti@gmail.com>
2022-04-15 12:14:32 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 //Coincode api used to get crypto prices
 
 function getFetch(){
-  	fetch("https://api.coincap.io/v2/assets")
+  	fetch("https://api.coincap.io/v2/assets/bitcoin")
       	.then(res => res.json())
       	.then(data => {
       	console.log(data)
+		document.querySelector('#btc').innerText = data.data.priceUsd
+		let percent = data.data.changePercent24Hr
+		if (percent >= 0){
+			document.getElementById("btcChange").style.color = "Green"
+			document.querySelector('#btcChange').innerText = percent
+		} else {
+			document.getElementById("btcChange").style.color = "Red"
+			document.querySelector('#btcChange').innerText = percent
+		}
       	})
     .catch(err => {
         console.log(`error ${err}`)
