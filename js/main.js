652ba8e389750d69dd235d339d30fc6a5b70ea6f
Kevin Halliwell <godzirrasti@gmail.com>
2022-04-18 23:35:33 -0400

    updates
    added back api call to fall back. polinex api does not work in certain scenarios. added shiba decimal helper

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@ let symbol = "" // string for symbol cell
 let coinHigh = "" // string for 24 hr high
 let coinLow = "" // string for 24 hr high
 let coinVolume = "" // string for volume
+let columnHigh = "24hr High" // string to delete column
+let columnLow = "24hr Low" // string to delete column
+let table = "crypto" // string for table. used to delete column
+let shibaTrue = true // boolean to adjust number of zeros
+
+// delete table column if api fails to connect
+function deleteColumn(table, column){
+	var table = document.getElementById(table),
+    rows = table.rows;
+
+	for (var i = 0; i < rows[0].cells.length; i++) {
+    	var str = rows[0].cells[i].innerHTML;
+    	if (str.search(column) != -1) {
+        	for (var j = 0; j < rows.length; j++) {
+           		rows[j].deleteCell(i);
+        }
+    }
+}
+}
 
 // helper function to get name and symbol
 function coinData(d, coin, num, symbol){
@@ -15,27 +34,75 @@ function coinData(d, coin, num, symbol){
 	document.querySelector('#' + symbol).innerText = d.data[num].symbol
 }
 
-// helper function to get the complete coin data
-function coinDataComplete(coinPrice, coinPercent, coinTest, coinHigh, coinLow, coinVolume){
-	let p = Number(coinTest.percentChange).toFixed(2)
+// backup helper function to get the complete coin data
+function coinDataComplete(coinPrice, coinPercent, coinData, coinHigh, coinLow, coinVolume, shibaTrue){
+	
+	// changes the box color base on percent change
+	let p = Number(coinData.percentChange).toFixed(2)
 	if (p >= 0){
 		document.getElementById(coinPercent).style.backgroundColor = "Green"
 	} else {
 		document.getElementById(coinPercent).style.backgroundColor = "Red"
 	}
-	// need to add if statement to add zeros for currency like shiba
-	document.querySelector('#' + coinPrice).innerText = "$" + Number(coinTest.last).toFixed(2)
 
+	// checks to see if currency needs more places beyond decimal
+	if (!shibaTrue) {
+		document.querySelector('#' + coinPrice).innerText = "$" + Number(coinData.last).toFixed(2)
+	} else {
+		document.querySelector('#' + coinPrice).innerText = "$" + Number(coinData.last).toFixed(8)
+	}
+
+	// adds the percent to the table
 	document.querySelector('#' + coinPercent).innerText = p + "%"
 
-	// need to add if statement to add zeros for currency like shiba
-	document.querySelector('#' + coinHigh).innerText = "$" + Number(coinTest.high24hr).toFixed(2)
+	// checks to see if currency needs more places beyond decimal
+	if (!shibaTrue) {
+		document.querySelector('#' + coinHigh).innerText = "$" + Number(coinData.high24hr).toFixed(2)
+	} else {
+		document.querySelector('#' + coinHigh).innerText = "$" + Number(coinData.high24hr).toFixed(7)
+	}
 
-	// need to add if statement to add zeros for currency like shiba
-	document.querySelector('#' + coinLow).innerText = "$" + Number(coinTest.low24hr).toFixed(2)
+	// checks to see if currency needs more places beyond decimal
+	if (!shibaTrue) {
+		document.querySelector('#' + coinLow).innerText = "$" + Number(coinData.high24hr).toFixed(2)
+	} else {
+		document.querySelector('#' + coinLow).innerText = "$" + Number(coinData.high24hr).toFixed(7)
+	}
 	
+	//************************************
 	// need to add statement to add k or m to currency
-	document.querySelector('#' + coinVolume).innerText = Number(coinTest.baseVolume).toFixed(2)
+	document.querySelector('#' + coinVolume).innerText = Number(coinData.baseVolume).toFixed(2)
+}
+
+// backup helper function to get coin data based on the first api
+function coinDataBackup(data, coin, num, symbol, coinPrice, coinPercent, coinVolume, shibaTrue){
+	document.querySelector('#' + coin).innerText = data.data[num].name
+	document.querySelector('#' + symbol).innerText = data.data[num].symbol
+
+	// changes the box color base on percent change
+	let p = Number(data.data[num].changePercent24Hr).toFixed(2)
+	if (p >= 0) {
+		document.getElementById(coinPercent).style.backgroundColor = "Green"
+	} else {
+		document.getElementById(coinPercent).style.backgroundColor = "Red"
+	}
+
+	// adds the percent to the table
+	document.querySelector('#' + coinPercent).innerText = p + "%"
+
+	// checks to see if currency needs more places beyond decimal
+	if (!shibaTrue) {
+		document.querySelector('#' + coinPrice).innerText = "$" + Number(data.data[num].priceUsd).toFixed(2)
+	} else {
+		document.querySelector('#' + coinPrice).innerText = "$" + Number(data.data[num].priceUsd).toFixed(7)
+	}
+
+	//************************************
+	// need to add statement to add k or m to currency
+	document.querySelector('#' + coinVolume).innerText = Number(data.data[num].volumeUsd24Hr).toFixed(2)
+
+	deleteColumn(table, columnHigh)
+	deleteColumn(table, columnLow)
 }
 
 // gets coin name and symbol
@@ -62,6 +129,7 @@ function getFetch(){
 			num = 14
 			symbol = "shibSymbol"
 			coinData(data, coin, num, symbol)
+			
 		})
   	.catch(err => {
 	  	console.log(`error ${err}`)
@@ -70,17 +138,18 @@ function getFetch(){
 
 //Get complete coin data
 function getFetchCoinData(){
-	fetch("https://poloniex.com/public?command=returnTicker")
+	fetch("https://poloniex.com/public?command=returnTicke")
 		.then(res => res.json())
 		.then(data => {
-			console.log(data)
+			//console.log(data)
 				//Bitcoin data
 				coinPrice = "btcPrice"
 				coinPercent = "btcPercent"
 				coinHigh = "btcHigh"
 				coinLow = "btcLow"
 				coinVolume = "btcVolume"
-				coinDataComplete(coinPrice, coinPercent, data.USDT_BTC, coinHigh, coinLow, coinVolume)
+				shibaTrue = false
+				coinDataComplete(coinPrice, coinPercent, data.USDT_BTC, coinHigh, coinLow, coinVolume, shibaTrue)
 
 				//Ethereum data
 				coinPrice = "ethPrice"
@@ -88,7 +157,8 @@ function getFetchCoinData(){
 				coinHigh = "ethHigh"
 				coinLow = "ethLow"
 				coinVolume = "ethVolume"
-				coinDataComplete(coinPrice, coinPercent, data.USDT_ETH, coinHigh, coinLow, coinVolume)
+				shibaTrue = false
+				coinDataComplete(coinPrice, coinPercent, data.USDT_ETH, coinHigh, coinLow, coinVolume, shibaTrue)
 
 				//Shiba-Inu data
 				coinPrice = "shibPrice"
@@ -96,7 +166,53 @@ function getFetchCoinData(){
 				coinHigh = "shibHigh"
 				coinLow = "shibLow"
 				coinVolume = "shibVolume"
-				coinDataComplete(coinPrice, coinPercent, data.USDT_SHIB, coinHigh, coinLow, coinVolume)
+				shibaTrue = true
+				coinDataComplete(coinPrice, coinPercent, data.USDT_SHIB, coinHigh, coinLow, coinVolume, shibaTrue)
+		})
+  	.catch(err => {
+	  	console.log(`error ${err}`)
+		// call the backup helper if the api fails
+		getFetchBackup()
+  	});
+}
+
+// gets coin name and symbol if the second api fails
+function getFetchBackup(){
+	fetch("https://api.coincap.io/v2/assets")
+		.then(res => res.json())
+		.then(data => {
+			//console.log(data)
+
+			//Bitcoin data
+			coin = "btc"
+			num = 0
+			symbol = "btcSymbol"
+			coinPrice = "btcPrice"
+			coinPercent = "btcPercent"
+			coinVolume = "btcVolume"
+			shibaTrue =  false
+			coinDataBackup(data, coin, num, symbol, coinPrice, coinPercent, coinVolume, shibaTrue)
+
+			// //Ethereum data
+			coin = "eth"
+			num = 1
+			symbol = "ethSymbol"
+			coinPrice = "ethPrice"
+			coinPercent = "ethPercent"
+			coinVolume = "ethVolume"
+			shibaTrue = false
+			coinDataBackup(data, coin, num, symbol, coinPrice, coinPercent, coinVolume, shibaTrue)
+
+			// //Shiba-Inu data
+			coin = "shib"
+			num = 14
+			symbol = "shibSymbol"
+			coinPrice = "shibPrice"
+			coinPercent = "shibPercent"
+			coinVolume = "shibVolume"
+			shibaTrue = true
+			coinDataBackup(data, coin, num, symbol, coinPrice, coinPercent, coinVolume, shibaTrue)
+
 		})
   	.catch(err => {
 	  	console.log(`error ${err}`)
